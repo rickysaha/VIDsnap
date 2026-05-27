0dae39b8ed3b1ebde80db934f6a4eaf88afc0490
priyanshu saha <priyanshusaha@priyanshus-MacBook-Air-2.local>
2026-05-27 13:55:23 +0530

    Fixed render deployment

--- main.py
+++ main.py
@@ -45,4 +45,9 @@ def gallery():
     print(reels)
     return render_template("gallery.html", reels=reels)
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 app.run(debug=True)
