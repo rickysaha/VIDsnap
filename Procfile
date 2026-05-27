431681504a6b05bb722131aafb5c55f428915fc7
priyanshu saha <priyanshusaha@priyanshus-MacBook-Air-2.local>
2026-05-27 19:20:06 +0530

    Added Railway deployment files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python main.py
+worker: python generate_process.py
--- supabase_client.py
+++
@@ -1,6 +0,0 @@
-from supabase import create_client
-
-url = "https://rsbugkmfzgbctsevvcvm.supabase.co"
-key = "sb_publishable__pDdQorKDRX19Yc6sk2E7w_YQkiRYp0"
-
-supabase = create_client(url, key)
